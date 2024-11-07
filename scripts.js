a819d95e3a70f505d3477f0ddb7cc47d9716aec9
Sherwal Hannays <100770363+ihyshan@users.noreply.github.com>
2024-11-07 09:14:36 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,10 @@
-/* Add your JavaScript to this file */
+document.querySelector('form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    const emailInput = document.getElementById('email');
+    const messageDiv = document.querySelector('.message');
+    if (emailInput.value) {
+        messageDiv.textContent = `Thank you! Your email address ${emailInput.value} has been added to our mailing list!`;
+    } else {
+        messageDiv.textContent = 'Please enter a valid email address.';
+    }
+});
